27a304a2b4b253eff66156f171963562ded19fb3
rescue88 <finik848848@gmail.com>
2020-11-04 13:38:13 +0200

    Added a web-routing to change pages without reloading.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
+// styles
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/Nav';
-import Profile from './components/Profile';
+// routing packages
+import { BrowserRouter, Route } from 'react-router-dom';
+
+// same components
+import Header from './components/Header/Header';
+import Navbar from './components/Nav/Nav';
+// click and change component
+import Profile from './components/Profile/Profile';
+import Dialogues from './components/Dialogues/Dialogues';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
+import Music from './components/Music/Music';
 
 const App = () => {
   return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper-content">
+          <Route path="/profile" component={Profile} />
+          <Route path="/dialogs" component={Dialogues} />
+          <Route path="/news" component={News} />
+          <Route path="/music" component={Music} />
+          <Route path="/settings" component={Settings} />
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 

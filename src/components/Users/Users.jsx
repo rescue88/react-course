697eab56ec9bfe4d67d5b75bc49a4fb85e3314b8
rescue88 <finik848848@gmail.com>
2020-11-14 21:56:54 +0200

    new class component Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,39 +1,50 @@
+import React from 'react';
+import * as axios from 'axios';
 import st from './Users.module.css';
+import userPhoto from './../../assets/images/Annie.png';
 
-const Users = (props) => {
-    let usersList = props.users.map( item => {
-        return <div key={ item.id } className={ st.userItem }>
-                <div className={ st.userItemFollow }>
-                    <div className={st.userItemFollowAva }>
-                        <img src={ item.photoLink } />
-                    </div>
-                    <div className={ st.userItemFollowButton }>
-                    { item.isFollowed ? <button onClick = { () => { props.onUnFollowClick(item.id) }}>Follow</button>: <button onClick={ () => props.onFollowClick(item.id) } >Unfollow</button> }
-                    </div>
-                </div>
-                <div className={ st.userItemInfo }>
-                    <div className={ st.userItemInfoDetect }>
-                        <span>{ item.fullName }</span>
-                        <span>{ item.status }</span>
-                    </div>
-                    <div className={ st.userItemInfoLocation }>
-                        <span>{ item.location.country },</span>
-                        <span>{ item.location.city }</span>
-                    </div>
-                </div>
-            </div>
-    });
-    return <div className={st.usersContainer}>
+class Users extends React.Component {
+    constructor(props) {
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items);
+        });
+    }
+    //add users method
+    render = () => {
+        return <div className={st.usersContainer}>
                 <div className={st.usersContainerHeader}>
                     Users list
                 </div>
                 <div className={st.usersContainerItems}>
-                    { usersList }
+                { this.props.users.map( item => {
+                    return <div key={ item.id } className={ st.userItem }>
+                            <div className={ st.userItemFollow }>
+                                <div className={st.userItemFollowAva }>
+                                    <img src={ item.photos.small != null ? item.photos.small : userPhoto } />
+                                </div>
+                                <div className={ st.userItemFollowButton }>
+                                { item.followed ? <button onClick = { () => { this.props.onUnFollowClick(item.id) }}>Follow</button> : <button onClick={ () => this.props.onFollowClick(item.id) } >Unfollow</button> }
+                                </div>
+                            </div>
+                            <div className={ st.userItemInfo }>
+                                <div className={ st.userItemInfoDetect }>
+                                    <span>{ item.name }</span>
+                                    <span>{ item.status }</span>
+                                </div>
+                                <div className={ st.userItemInfoLocation }>
+                                    <span>{ 'item.location.country' },</span>
+                                    <span>{ 'item.location.city' }</span>
+                                </div>
+                            </div>
+                        </div>
+                })}
                 </div>
                 <div className={st.usersContainerShowMore}>
                     <button>Show more</button>
                 </div>
             </div>
+    };
 }
 
 export default Users;
--- src/redux/usersReducer.js
+++ src/redux/usersReducer.js
@@ -3,41 +3,9 @@ const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET-USERS';
 
-let initialState = {
-    users: [
-        {
-            id: 1,
-            photoLink: 'https://thumbor.forbes.com/thumbor/fit-in/416x416/filters%3Aformat%28jpg%29/https%3A%2F%2Fspecials-images.forbesimg.com%2Fimageserve%2F5ec595d45f39760007b05c07%2F0x0.jpg%3Fbackground%3D000000%26cropX1%3D989%26cropX2%3D2480%26cropY1%3D74%26cropY2%3D1564',
-            isFollowed: true, fullName: 'Bohdan Z.',
-            status: 'I am a boss',
-            location: {city: 'Romny', country: 'Ukraine'}
-        },
-        {
-            id: 2,
-            photoLink: 'https://static.wikia.nocookie.net/swordartonline/images/d/d4/Alice_Synthesis_Thirty.png/revision/latest?cb=20190127122239',
-            isFollowed: false,
-            fullName: 'Alice A.',
-            status: 'Foreign languages and daily talk',
-            location: {city: 'Istanbul', country: 'Turkey'}
-        },
-        {
-            id: 3,
-            photoLink: 'https://practicaltyping.com/wp-content/uploads/2019/02/okabe.jpg',
-            isFollowed: false,
-            fullName: 'Okabe R.',
-            status: 'Ohhh, timetraveling',
-            location: {city: 'Japan', country: 'Nagasaki'}
-        },
-        {
-            id: 4,
-            photoLink: 'https://kg-portal.ru/img/84693/main.jpg',
-            isFollowed: true,
-            fullName: 'Arthur L.',
-            status: 'Im gay cuz i like football',
-            location: {city: 'Kharkiv', country: 'Ukraine'}
-        },
-    ]
-};
+let initialState = { 
+    users: [],
+ };
 
 //action creator template for following function
 export const followCreator = (userId) => {
@@ -69,7 +37,7 @@ const usersReducer = (state = initialState, action) => {
                 ...state,
                 users: [...state.users].map( item => {
                     if(item.id === action.userId) {
-                        return {...item, isFollowed: true,};
+                        return {...item, followed: true,};
                     }
                     return item;
                 }),
@@ -80,7 +48,7 @@ const usersReducer = (state = initialState, action) => {
                 ...state,
                 users: [...state.users].map( item => {
                     if(item.id === action.userId) {
-                        return {...item, isFollowed: false,};
+                        return {...item, followed: false,};
                     }
                     return item;
                 })
@@ -88,7 +56,6 @@ const usersReducer = (state = initialState, action) => {
         //initial users adding, get more users by a click
         case SET_USERS:
             return {
-                ...state,
                 users: [...state.users, ...action.users],
             };
         //return unchanged original state if there is not a users action

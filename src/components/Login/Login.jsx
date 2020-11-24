9d7039f63a009b76235665740c56c96d001fec0e
rescue88 <finik848848@gmail.com>
2020-11-24 16:18:14 +0200

    redux-form library - first try

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,38 @@
+import { Field, reduxForm } from 'redux-form';
 import st from './Login.module.css';
 
-const Login = (props) => {
+const LoginForm = (props) => {
     return (
-        <div className={ st.login }>Login</div>
+        <form onSubmit={ props.handleSubmit }>
+            <div htmlFor="login">Enter login
+                <Field name="login" component="input" placeholder="Login" />
+            </div>
+            <div htmlFor="password">Enter password
+                <Field name="password" component="input" placeholder="Password" />
+            </div>
+            <div htmlFor="rememberMe">Remember me
+                <Field name="rememberMe" component="input" type="checkbox" />
+            </div>
+            <div className={ st.submitBtn }>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({
+    form: 'login',
+})(LoginForm);
+
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData);
+    }
+    return (
+        <div className={ st.login }>
+            <h1>Login</h1>
+            <LoginReduxForm onSubmit={ onSubmit } />
+        </div>
     );
 }
 
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,6 +3,7 @@ import dialoguesReducer from "./dialoguesReducer";
 import profileReducer from "./profileReducer";
 import usersReducer from "./usersReducer";
 import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 const { createStore, combineReducers, applyMiddleware } = require("redux");
 
 let reducers = combineReducers({
@@ -10,6 +11,7 @@ let reducers = combineReducers({
     dialoguesPage: dialoguesReducer,
     usersPage: usersReducer,
     auth: authReducer,
+    form: formReducer,
 });
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
